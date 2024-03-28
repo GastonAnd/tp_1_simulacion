c686e909e68551cd8e9c22d9c7bae8dd6ebd3dcd
gaston andrada <gasti.andrada123@gmail.com>
2024-03-28 13:07:39 -0300

    Agregado Promedio

--- ruleta.py
+++ ruleta.py
@@ -1,6 +1,6 @@
 import random
 import matplotlib.pyplot as plt
-
+import numpy as np
 num_corridas = int(input("Ingrese corridas: "))
 num_valores = int(input("Ingrese tiradas: "))
 num_elegido = int(input("Número a elegir: "))
@@ -10,6 +10,8 @@ def tirar_ruleta():
 
 # Lista para almacenar los resultados de las corridas
 resultados_corridas = []
+num_ruleta=np.arange(37)
+promedio_esperado=np.mean(num_ruleta)
 # Realizar  corridas
 for i in range(num_corridas):
     frec_absol_elegido = []
@@ -17,26 +19,17 @@ for i in range(num_corridas):
     resultados_corrida = []
     vpn=[]
     # Realizar  tiradas en la corrida actual
-    for _ in range(num_valores):
+    for j in range(num_valores):
         resultado = tirar_ruleta()
         resultados_corrida.append(resultado)
         if resultado == num_elegido:
           frec_absol_elegido.append(1)
         else:
            frec_absol_elegido.append(0)
-        vpn.append(resultado / num_valores)        
+        vpn.append(np.mean(resultados_corrida[:j+1]))      
     # Agregar los resultados de la corrida actual a la lista de resultados de corridas
     
     frec_relativa = [sum(frec_absol_elegido[:i+1]) / (i+1) for i in range(num_valores)]
-    prom=sum(resultados_corrida[:i+1])/ num_valores 
-    print(vpn)
-
-    for i, corrida in enumerate(resultados_corridas):
-      print(f"Corrida {i+1}: {corrida}")
-   
-    print("Promedio",prom)
-  
-
     #### GRAFICOS ##########
     plt.plot(range(1, num_valores + 1), frec_relativa, label='Frecuencia Relativa', color='red')
 
@@ -49,7 +42,7 @@ for i in range(num_corridas):
     
     
     plt.plot(range(1, num_valores + 1), vpn, label='Promedio', color='red')
-    plt.axhline(y=prom, color='blue', linestyle='--')
+    plt.axhline(y=promedio_esperado, color='blue', linestyle='--')
     plt.xlabel('Número de tiradas')
     plt.ylabel('Promedio de las Tiradas')
     plt.title(f'Promedio de los numeros en {num_valores} tiradas')
