154787129238eaa50df720093604f0d162149fdb
gaston andrada <gasti.andrada123@gmail.com>
2024-05-12 21:50:26 -0300

    Frecuencia relativa agregada

--- ruleta_1.2.py
+++ ruleta_1.2.py
@@ -12,9 +12,10 @@ num_corridas = int(args[1])
 cant_tiradas = int(args[3])
 capital = (args[5])
 estrategia=(args[7])
-
-monto_inicial=5000
-apuesta_gral=150
+rojos = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+negros = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+monto_inicial=500
+apuesta_gral=15
 def tirar_ruleta():
     return random.randint(0, 36)
 def seq_fibonacci(n):
@@ -24,10 +25,7 @@ def seq_fibonacci(n):
         fib_sequence.append(next_fib)
     return fib_sequence
 
-rojos = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
-negros = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
-frecuencia=[]
-montoTotal=[]
+
 def martingala():
     ganancia=0
     monto=monto_inicial
@@ -104,14 +102,13 @@ def dalembert():
             frecuencia.append(contador_tirada)
             contador_tirada=0
             montoTotal.append(monto)
-            
         else:
             print("perdiste la apuesta jugando,",apuesta)
             apuesta+=apuesta_gral 
             monto-=apuesta
             print("tu monto ahora es",monto)
             montoTotal.append(monto)
-            if monto < apuesta_gral:
+            if monto < apuesta:
                 print("te quedaste sin dinsero")
                 break
     print(montoTotal)
@@ -124,7 +121,22 @@ def dalembert():
         if cant_tiradas==0:
             if resultado in rojos:
                 """
-
+"""def labouchere(serie):
+    apuesta = serie[0] + serie[-1]
+    while len(serie) > 0:
+        if len(serie) == 1:
+            apuesta += serie[0]
+        print("Apuesta:", apuesta)
+        resultado = tirar_ruleta()  
+        if resultado == "gane":
+            del serie[0]
+            del serie[-1]
+            apuesta = serie[0] + serie[-1]
+        else:
+            print("Valí")
+            serie.append(apuesta)
+            apuesta += serie[0] + serie[-1]
+"""
 def martingala_infi():
     ganancia=0
     monto=monto_inicial
@@ -202,75 +214,87 @@ def dalembert_infi():
     print(frecuencia)
     print(montoTotal)
 
-for i in range(num_corridas):
+for i in range(1,num_corridas+1):
+    frecuencia=[]
+    montoTotal=[]
     if estrategia =='m' and capital=='f':
         martingala()
-        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True)        
-        plt.bar(vu_MG, fr_MG, color='blue')
+        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True)   
+        fr_rel_MG=fr_MG/  np.sum(fr_MG)     
+        plt.bar(vu_MG, fr_rel_MG, color='blue')
         plt.xlabel('Tiradas desde la última ganancia')
         plt.ylabel('Frecuencia')
         plt.title('Frecuencia de tiradas desde la última ganancia')
         plt.show()
-
         plt.plot(montoTotal,color='red')
         plt.axhline(y=monto_inicial, color='blue', linestyle='--')
+        plt.savefig("grafica_flujo_caja_martingala.png")
+
         plt.show()
 
     elif estrategia=='f' and capital=='f' : 
         fibonacci()
         vu_FIB, fr_FIB=np.unique(frecuencia,return_counts=True)
-        plt.bar(vu_FIB, fr_FIB, color='blue')
+        fr_rel_FIB=fr_FIB/  np.sum(fr_FIB)     
+        plt.bar(vu_FIB, fr_rel_FIB, color='blue')
         plt.xlabel('Tiradas desde la última ganancia')
         plt.ylabel('Frecuencia')
         plt.title('Frecuencia de tiradas desde la última ganancia')
         plt.show()
         plt.plot(montoTotal,color='red')
         plt.axhline(y=monto_inicial, color='blue', linestyle='--')
-        plt.savefig("grafica_flujo_caja.png")
+        plt.savefig("grafica_flujo_caja_fibonacci.png")
         plt.show()
     elif estrategia=='f' and capital=='i' : 
         fibonacci_infi()
         vu_FIB, fr_FIB=np.unique(frecuencia,return_counts=True)
-        plt.bar(vu_FIB, fr_FIB, color='blue')
+        fr_rel_FIB=fr_FIB/  np.sum(fr_FIB)     
+        plt.bar(vu_FIB, fr_rel_FIB, color='blue')
         plt.xlabel('Tiradas desde la última ganancia')
         plt.ylabel('Frecuencia')
         plt.title('Frecuencia de tiradas desde la última ganancia')
         plt.show()
         plt.plot(montoTotal,color='red')
         plt.axhline(y=monto_inicial, color='blue', linestyle='--')
+        plt.axhline(y=0, color='green', linestyle='--')
+        plt.savefig("grafica_flujo_caja_fibonacci_infinito.png")
         plt.show()
     elif estrategia=='d' and capital=='f':
         dalembert()
         vu_DAL,fr_DAL=np.unique(frecuencia,return_counts=True)
-        plt.bar(vu_DAL,fr_DAL,color='blue')
+        fr_rel_DAL=fr_DAL/  np.sum(fr_DAL)     
+        plt.bar(vu_DAL,fr_rel_DAL,color='blue')
         plt.xlabel('Tiradas desde la última ganancia')
         plt.ylabel('Frecuencia')
         plt.title('Frecuencia de tiradas desde la última ganancia')
         plt.show()
         plt.plot(montoTotal,color='red')
-
         plt.axhline(y=monto_inicial, color='blue', linestyle='--')
+        plt.savefig("grafica_flujo_caja_dalembert.png")
         plt.show()
     elif estrategia=='d' and capital=='i':
         dalembert_infi()
         vu_DAL,fr_DAL=np.unique(frecuencia,return_counts=True)
-        plt.bar(vu_DAL,fr_DAL,color='blue')
+        fr_rel_DAL=fr_DAL/  np.sum(fr_DAL)     
+        plt.bar(vu_DAL,fr_rel_DAL,color='blue')
         plt.xlabel('Tiradas desde la última ganancia')
         plt.ylabel('Frecuencia')
         plt.title('Frecuencia de tiradas desde la última ganancia')
         plt.show()
         plt.plot(montoTotal,color='red')
         plt.axhline(y=monto_inicial, color='blue', linestyle='--')
+        plt.savefig("grafica_flujo_caja_dalembert_infito.png")        
         plt.show()
     elif estrategia == 'm' and capital =='i':
         martingala_infi()
-        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True)        
-        plt.bar(vu_MG, fr_MG, color='blue')
+        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True) 
+        fr_rel_MG=fr_MG/  np.sum(fr_MG)    
+        plt.bar(vu_MG, fr_rel_MG, color='blue')
         plt.xlabel('Tiradas desde la última ganancia')
         plt.ylabel('Frecuencia')
         plt.title('Frecuencia de tiradas desde la última ganancia')
         plt.show()
-
         plt.plot(montoTotal,color='red')
         plt.axhline(y=monto_inicial, color='blue', linestyle='--')
+        plt.savefig("grafica_flujo_caja_martingala_infito.png")
         plt.show()
