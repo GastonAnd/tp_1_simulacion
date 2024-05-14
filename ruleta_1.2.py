d29fc9e7a452abe5ac58b973dcb134b1f58599d2
gaston andrada <gasti.andrada123@gmail.com>
2024-05-14 13:01:21 -0300

    Modificación de funciones para crear las graficas

--- ruleta_1.2.py
+++ ruleta_1.2.py
@@ -99,13 +99,14 @@ def labouchere():
     secuencia = secuencia_inicial.copy()
     frecuencia = []
     montoTotal = []
+    global tiradas
     contador_tirada = 0
     for i in range(cant_tiradas):
         if len(secuencia) == 0:
             secuencia = secuencia_inicial.copy()  # Reiniciar la secuencia
         apuesta = secuencia[0] if len(secuencia) == 1 else secuencia[0] + secuencia[-1]
         if monto < apuesta:
-            print(f"No hay suficiente capital para continuar después de {i} tiradas.")
+            tiradas=i
             break
         resultado = tirar_ruleta()
         contador_tirada += 1
@@ -205,167 +206,78 @@ def labouchere_infi():
 
         montoTotal.append(monto)
     return montoTotal, frecuencia
-for i in range(1,num_corridas+1):
+
+def plot_flujo_caja(montoTotalCorrida,title,filename):
+    for i, montoTotal in enumerate(montoTotalCorrida):
+        plt.plot(montoTotal,label=f'Flujo de Caja en corrida {i +1}')
+    plt.xlabel('Numero de Tiradas')
+    plt.ylabel('Cantidad de Capital')
+    plt.title(title)
+    plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
+    plt.legend()
+    plt.savefig(filename)
+    plt.show()
+
+def plot_frecuencia_relativa(frecuencia,tiradas,corrida):
+    vu, fr=np.unique(frecuencia,return_counts=True)   
+    frec_rel=[]    
+    for i in range(0,len(fr)):
+        frec_rel.append(fr[i]/tiradas) 
+    plt.bar(vu, frec_rel, color='blue')
+    plt.xlabel('Numero de Tiradas')
+    plt.ylabel('Frecuencia Relativa')
+    plt.ylim(0,0.25)
+    plt.title('Frecuencia relativa de obtener apuesta favorable')
+    plt.savefig(f'grafica_frecuencia_relativa_estrategia_{estrategia}_capita_{capital}_corrida_{corrida}.png')        
+    plt.show()
+def plot_frecuencia_relativa_infi(frecuencia,corrida):
+    vu, fr=np.unique(frecuencia,return_counts=True)   
+    frec_rel=[]    
+    for i in range(0,len(fr)):
+        frec_rel.append(fr[i]/cant_tiradas) 
+    plt.bar(vu, frec_rel, color='blue')
+    plt.xlabel('Numero de Tiradas')
+    plt.ylabel('Frecuencia Relativa')
+    plt.ylim(0,0.25)
+    plt.title('Frecuencia relativa de obtener apuesta favorable')
+    plt.savefig(f'grafica_frecuencia_relativa_estrategia_{estrategia}_capital_{capital}_corrida_{corrida}.png')        
+    plt.show()    
+montoTotalCorrida=[]
+for corrida in range(1,num_corridas+1):
     frecuencia=[]
     montoTotal=[]
+    corrida=corrida
     if estrategia =='m' and capital=='f':
         martingala()
-        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True)   
-        frec_rel_MG=[]    
-        for i in range(0,len(fr_MG)):
-            frec_rel_MG.append(fr_MG[i]/tiradas) 
-        plt.bar(vu_MG, frec_rel_MG, color='blue')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_martingala_finito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_martingala.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa(frecuencia,tiradas,corrida)
     elif estrategia=='f' and capital=='f' : 
         fibonacci()
-        vu_FIB, fr_FIB=np.unique(frecuencia,return_counts=True)
-        fr_rel_FIB=fr_FIB/  np.sum(fr_FIB)     
-        frec_rel_FIB=[]    
-        for i in range(0,len(fr_FIB)):
-            frec_rel_FIB.append(fr_FIB[i]/tiradas)
-        plt.bar(vu_FIB, fr_rel_FIB, color='blue')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_fibonacci_finito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_fibonacci.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa(frecuencia,tiradas,corrida)
     elif estrategia=='f' and capital=='i' : 
         fibonacci_infi()
-        vu_FIB, fr_FIB=np.unique(frecuencia,return_counts=True)
-        frec_rel_FIB=[]    
-        for i in range(0,len(fr_FIB)):
-            frec_rel_FIB.append(fr_FIB[i]/cant_tiradas)
-        plt.bar(vu_FIB, frec_rel_FIB, color='blue')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_fibonacci_infinito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_fibonacci_infinito.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa_infi(frecuencia,corrida)
     elif estrategia=='d' and capital=='f':
         dalembert()
-        vu_DAL,fr_DAL=np.unique(frecuencia,return_counts=True)
-        frec_rel_DAL=[]    
-        for i in range(0,len(fr_DAL)):
-            frec_rel_DAL.append(fr_DAL[i]/tiradas)
-        plt.bar(vu_DAL, frec_rel_DAL, color='blue')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_dalembert_finito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_dalembert.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa(frecuencia,tiradas,corrida)
     elif estrategia=='d' and capital=='i':
         dalembert_infi()
-        vu_DAL,fr_DAL=np.unique(frecuencia,return_counts=True)
-        frec_rel_DAL=[]    
-        for i in range(0,len(fr_DAL)):
-            frec_rel_DAL.append(fr_DAL[i]/cant_tiradas)
-        plt.bar(vu_DAL, frec_rel_DAL, color='blue')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_dalembert_infinito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_dalembert_infinito.png")        
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa_infi(frecuencia,corrida)
     elif estrategia == 'm' and capital =='i':
         martingala_infi()
-        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True) 
-        frec_rel_MG=[]    
-        for i in range(0,len(fr_MG)):
-            frec_rel_MG.append(fr_MG[i]/cant_tiradas)
-        plt.bar(vu_MG, frec_rel_MG, color='blue')    
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_martingala_infinito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_martingala_infito.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa_infi(frecuencia,corrida)
     elif estrategia=='l' and capital=='f':
         montoTotal,frecuencia=labouchere()
-        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True) 
-        frec_rel_MG=[]    
-        for i in range(0,len(fr_MG)):
-            frec_rel_MG.append(fr_MG[i]/cant_tiradas)
-        plt.bar(vu_MG, frec_rel_MG, color='blue')    
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_labouchere_finito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_laboucher_finito.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa(frecuencia,tiradas,corrida)
     elif estrategia=='l' and capital=='i':
         montoTotal,frecuencia=labouchere_infi()
-        vu_MG, fr_MG=np.unique(frecuencia,return_counts=True) 
-        frec_rel_MG=[]    
-        for i in range(0,len(fr_MG)):
-            frec_rel_MG.append(fr_MG[i]/cant_tiradas)
-        plt.bar(vu_MG, frec_rel_MG, color='blue')    
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Frecuencia Relativa')
-        plt.title('Frecuencia relativa de obtener apuesta favorable')
-        plt.savefig("grafica_frecuencia_relativa_labouchere_infinito.png")        
-        plt.show()
-        plt.plot(montoTotal,color='red',label='Flujo de Caja')
-        plt.xlabel('Numero de Tiradas')
-        plt.ylabel('Cantidad de Capital')
-        plt.title('Flujo de caja')
-        plt.axhline(y=monto_inicial, color='blue', linestyle='--',label='Flujo de Caja Inicial')
-        plt.legend()
-        plt.savefig("grafica_flujo_caja_laboucher_infinito.png")
-        plt.show()
+        montoTotalCorrida.append(montoTotal)
+        plot_frecuencia_relativa_infi(frecuencia,corrida)
+
+plot_flujo_caja(montoTotalCorrida,'Flujo de caja',f'flujo_caja_estrategia_{estrategia}_capital_{capital}')
