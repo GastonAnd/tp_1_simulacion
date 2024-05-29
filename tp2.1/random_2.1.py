7036438b55a8610f7791e65b4e05581e1fe79271
Ignacio Rodriguez <ignacio.rodriguez.sistemas@gmail.com>
2024-05-28 21:20:54 -0300

    20240528 RandomNumList

--- tp2.1/random_2.1.py
+++ tp2.1/random_2.1.py
@@ -1,20 +1,25 @@
 
 
+randomNumList =[]
+
+
 def Generador1(): #Generador de numeros pseudoaleatorios 1
+    global randomNumList
     print("Generador 1")
 
 def Generador2(): #Generador de numeros pseudoaleatorios 2
-    print("Generador 1") 
+    global randomNumList
+    print("Generador 2") 
 
 
 def Test1(): #Testeador 1
     print("Testeador 1")
 
 def Test2(): #Testeador 2
-    print("Testeador 1")
+    print("Testeador 2")
 
 def Test3(): #Testeador 3
-    print("Testeador 1")
+    print("Testeador 3")
 
 def Test4(): #Testeador 4
     print("Testeador 4")
